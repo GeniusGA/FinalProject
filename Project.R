fec08c96cdf572f929b07906d714337f6efc6c9e
Gene <49526712+GeniusGA@users.noreply.github.com>
2021-11-23 13:12:35 -0500

    Update Project.R

--- Project.R
+++ Project.R
@@ -4,3 +4,38 @@ head(data)
 m1 = lm(medv ~., data =data )
 summary(m1)
 anova(m1)
+
+### The relationship betweeen each variable
+pairs(data)
+
+# The 90% confidence interval for the variable
+confint(m1,level=0.9)
+
+
+par(mfrow=c(1,2))
+plot(fitted(m1), resid(m1), col = "grey", pch = 20,
+     xlab = "Fitted", ylab = "Residual",cex=2,
+     main = "Fitted versus Residuals")
+abline(h = 0, col = "darkorange", lwd = 2)
+qqnorm(resid(m1), col = "grey",pch=20,cex=2)
+qqline(resid(m1), col = "dodgerblue", lwd = 2)
+
+library(faraway)
+vif(m1)
+
+
+### backward Selection
+fit_back_aic = step(m1, direction = "backward")
+
+### Forward Selection
+fit_null = lm(medv~1,data=data)
+fit_forw_aic = step(fit_null, 
+                    scope = mdev ~ crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+black+lstat, 
+                    direction = "forward")
+
+### Stepwise Selection
+fit_null = lm(medv~1,data=data)
+fit_forw_aic = step(fit_null, 
+                    scope = mdev ~ crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+black+lstat, 
+                    direction = "both")
+
