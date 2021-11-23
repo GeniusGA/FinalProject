28e4eb2b5700b42aa112d22a4576160e17154dfc
Gene <49526712+GeniusGA@users.noreply.github.com>
2021-11-23 18:23:26 -0500

    Update Project.R

--- Project.R
+++ Project.R
@@ -24,6 +24,7 @@ library(faraway)
 vif(m1)
 
 
+
 ### backward Selection
 fit_back_aic = step(m1, direction = "backward")
 
@@ -39,3 +40,24 @@ fit_forw_aic = step(fit_null,
                     scope = mdev ~ crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+black+lstat, 
                     direction = "both")
 
+# After the Stepwise function
+fit_step = lm(medv~lstat + rm + ptratio + dis + nox + chas + black + zn+ crim+rad+tax,data = data)
+summary(fit_step)
+
+### Compute the VIF Score
+vif(fit_step)
+
+### Plot the graph
+par(mfrow=c(1,2))
+plot(fitted(fit_step), resid(fit_step), col = "grey", pch = 20,
+     xlab = "Fitted", ylab = "Residual",cex=2,
+     main = "Fitted versus Residuals")
+abline(h = 0, col = "darkorange", lwd = 2)
+qqnorm(resid(fit_step), col = "grey",pch=20,cex=2)
+qqline(resid(fit_step), col = "dodgerblue", lwd = 2)
+
+### Print the covariance matrixx for model parameter
+vcov(fit_step)
+
+layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
+plot(fit_step)
